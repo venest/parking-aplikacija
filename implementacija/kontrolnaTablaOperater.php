5dfd62467e7d049dfbca46b94d4c1aae77ca701f
venest <veljkonestorovic@gmail.com>
2020-05-14 15:19:26 +0200

    codeigniter4 pocetni commit

--- implementacija/kontrolnaTablaOperater.php
+++
@@ -1,8 +0,0 @@
-<div class="collapse text-center d-md-block" id="side-nav">
-    <h5 class="my-4">KONTROLNA TABLA</h5>
-    <div class="list-group text-center mb-2">
-        <a href="ulazakGost.php" class="list-group-item list-group-item-action py-4">ULAZAK GOST</a>
-        <a href="ulazakRegistrovani.php" class="list-group-item list-group-item-action py-4">ULAZAK REGISTROVANI</a>
-        <a href="izlazak.php" class="list-group-item list-group-item-action py-4">IZLAZAK</a>
-    </div>
-</div>
